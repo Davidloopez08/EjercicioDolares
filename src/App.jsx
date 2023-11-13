21a87990affca0936a48778cd9270c1ae4d7759c
David <davidlopezlin08@gmail.com>
2023-11-13 12:31:31 +0100

    0.1.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,35 @@
 import { useState } from 'react';
 import './App.css';
 
-const EURO_DOLAR = 1.2;
-//Aqui he puesto la diferencia entre el euro y el dolar
+const EURO_DOLAR = 1.2; // Debes definir el valor de conversión
 
-const CurrencyChanger = () => {
-  //Aqui creo los dos Hooks que pides, uno para la gestion de los euros y otro para la gestion de los dolares.
+const useCurrencyChangerState = () => {
   const [euros, setEuros] = useState(0);
   const [dolars, setDolars] = useState(0);
 
-  const onChangeInput = (e) => {
-    const newValue = e.target.value;
-    setEuros(newValue);
-    setDolars(newValue * EURO_DOLAR);
+  const onInputChange = (value, inputType) => {
+    if (inputType === 'euros') {
+      setEuros(value);
+      setDolars(value * EURO_DOLAR);
+    } else if (inputType === 'dolars') {
+      setDolars(value);
+      setEuros(value / EURO_DOLAR);
+    }
   };
+
+  return { euros, dolars, onInputChange };
+};
+
+const CurrencyChanger = () => {
+  const { euros, dolars, onInputChange } = useCurrencyChangerState();
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column', marginBottom: '50px' }}>
       <div>
-        Euros: <input type="number" onChange={onChangeInput} value={euros} /> €
-        Dólares: {dolars.toFixed(2)} $
+        Euros: <input style={{ width: '150px' }} type="number" step="0.01" value={euros} onChange={(e) => onInputChange(e.target.value, 'euros')} /> €
+      </div>
+      <div>
+        Dólares: <input style={{ width: '150px' }} type="number" step="0.01" value={dolars} onChange={(e) => onInputChange(e.target.value, 'dolars')} /> $
       </div>
     </div>
   );
